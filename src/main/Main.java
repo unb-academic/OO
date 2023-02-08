8a6299dd840c8958547029fedadb06ef903715bd
kyomi <personal@kyomi.codes>
2023-02-07 22:15:29 -0300

    Adicionar classe de view de login

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,17 @@
 package src.main;
 
+import src.views.Login;
+
+import java.awt.*;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, FontFormatException {
+        System.out.println("[banco] Criando as tabelas necessárias.");
         Banco.criarTabelas();
+
+        System.out.println("[view] Iniciando a view de login");
+        Login login = new Login();
+        login.mostrar();
     }
 }
---
+++ src/views/Login.java
@@ -0,0 +1,89 @@
+package src.views;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.IOException;
+
+public class Login extends JFrame {
+    private final JFrame frame;
+
+    public Login() throws IOException, FontFormatException {
+        super();
+
+        // Fonte
+        File arquivo = new File("assets/fonts/Poppins-Medium.ttf");
+        Font fonte = Font.createFont(Font.TRUETYPE_FONT, arquivo).deriveFont(15f);
+
+        JFrame frame = new JFrame();
+        frame.setSize(640, 360);
+        frame.setTitle("Zubank - Login");
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.getContentPane().setBackground(new Color(95, 159, 159));
+        frame.setLayout(null);
+
+        // Usuário
+        JLabel usuario = new JLabel("Usuário:");
+        usuario.setFont(fonte);
+        usuario.setBounds(5, 50, 120,30);
+        usuario.setForeground(Color.white);
+        frame.add(usuario);
+
+        JTextField usuarioField = new JTextField();
+        usuarioField.setText("");
+        usuarioField.setBounds(85, 50, 140, 30);
+        usuarioField.setVisible(true);
+        frame.add(usuarioField);
+
+        // Senha
+        JLabel senha = new JLabel("Senha:");
+        senha.setFont(fonte);
+        senha.setBounds(5, 115, 120,30);
+        senha.setForeground(Color.white);
+        frame.add(senha);
+
+        JPasswordField senhaField = new JPasswordField();
+        senhaField.setText("");
+        senhaField.setBounds(85, 115, 140, 30);
+        senhaField.setForeground(Color.black);
+        senhaField.setVisible(true);
+        frame.add(senhaField);
+
+        // Botão de 'login'
+        JButton entrar = new JButton("Entrar");
+        entrar.setFont(fonte);
+        entrar.setBounds(5, 225, 140, 35);
+        entrar.setBackground(Color.white);
+        entrar.setForeground(Color.black);
+        entrar.setVisible(true);
+        entrar.addActionListener(this::clickLogin);
+        frame.add(entrar);
+
+        // Botão de cadastro
+        JButton cadastrar = new JButton("Cadastrar-se");
+        cadastrar.setFont(fonte);
+        cadastrar.setBounds(155, 225, 140, 35);
+        cadastrar.setBackground(Color.white);
+        cadastrar.setForeground(Color.black);
+        cadastrar.setVisible(true);
+        cadastrar.addActionListener(this::clickCadastro);
+        frame.add(cadastrar);
+
+        this.frame = frame;
+    }
+
+    private void clickLogin(ActionEvent actionEvent) {
+        System.out.println("[view] Realizando login...");
+    }
+
+    private void clickCadastro(ActionEvent actionEvent) {
+        System.out.println("[view] Realizando cadastro...");
+    }
+
+    public void mostrar() {
+        this.frame.setVisible(true);
+    }
+}
